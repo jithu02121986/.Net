8ccaad5d2986cffa9786b9956f293afc2b3951e8
Jitendra Thota <jithunaidu02@gmail.com>
2025-01-06 20:06:23 +0530

    Day-18 commit

---
+++ Day-18/ComandLineArgs_Hello/CommandLineArgsEx.cs
@@ -0,0 +1,19 @@
+﻿class CommandLineArgsEx
+{
+    static void Main(string[] userNames)
+    {
+        Console.WriteLine($"The Size of User Names {userNames.Length}");
+        //for Loop
+
+        for (int i = 0; i < userNames.Length; i++)
+        {
+            Console.WriteLine("User Names : " + userNames[i]);
+        }
+        //for each loop
+        foreach (string userName in userNames)
+        {
+            Console.WriteLine("User Names in for each loop : :" + userName);
+        }
+
+    }
+}
